7501c75a5506e20b355d8afacacaecf21f816dfc
Abiyyirahinda <muhammadabiyyi73@gmail.com>
2025-08-21 14:03:52 +0700

    pm2 config

---
+++ ecosystem.config.js
@@ -0,0 +1,30 @@
+module.exports = {
+    apps: [
+        {
+            name: "flight-celery-worker",
+            script: "celery",
+            args: "celery_app.celery_app worker -l info --pool=solo",
+            interpreter: "none",
+            // restart_delay: 5000,
+            // max_restarts: 10,
+            // max_memory_restart: "500M"
+        },
+        {
+            name: "flight-celery-beat",
+            script: "celery",
+            args: "celery_app.celery_app beat -l info",
+            interpreter: "none",
+            // restart_delay: 5000,
+            // max_restarts: 10,
+            // max_memory_restart: "200M"
+        },
+        {
+            name: "cnote-update-schedule",
+            script: "app.py",
+            interpreter: "python",
+            // restart_delay: 5000,
+            // max_restarts: 10,
+            // max_memory_restart: "300M"
+        }
+    ]
+}
