ba19f14e4869e7b9cd6d7b3c31f79e70f1447ab9
Abiyyirahinda <muhammadabiyyi73@gmail.com>
2025-08-25 12:24:29 +0700

    env

--- sentry_config.py
+++ sentry_config.py
@@ -1,9 +1,10 @@
 import sentry_sdk 
+import os
 from sentry_sdk.integrations.celery import CeleryIntegration
 from flask import Flask
 
 sentry_sdk.init(
-    dsn="https://e1aa4aba2b7ab98cc2a7744f91ccd925@o4506467821092864.ingest.us.sentry.io/4509902539522049",
+    dsn=os.getenv("SENTRY_DSN"),
     integrations=[CeleryIntegration()],
     send_default_pii=True,
     traces_sample_rate=1.0,
